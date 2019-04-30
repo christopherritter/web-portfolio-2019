4a5e07589cd13a6c28b9f3e9459f61849425d40f
Christopher Ritter <chris@christopherritter.com>
2019-04-30 17:50:50 -0400

    view deliverable button

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -21,7 +21,7 @@
           href="#"
           class="btn btn-bubble btn-bubble-experience rounded-circle"
           role="button"
-          v-on:click="job.showTasks = !job.showTasks"
+          v-on:click="toggleTasks"
         ></a>
         <div class="btn btn-bubble-line"></div>
       </div>
@@ -64,7 +64,24 @@
             class="tl-task-desc clearfix"
             v-if="task.showTask === true"
           >
-            {{ task.description }}
+            <span class="tl-task-desc-lit">
+              {{ task.description }}
+            </span>
+            <a
+              href="#"
+              class="btn btn-view"
+              v-on:click="task.showDeliverable = !task.showDeliverable"
+            >
+              <svg
+                style="width:24px;height:24px"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#84A86B"
+                  d="M15.5,9C16.2,9 16.79,8.76 17.27,8.27C17.76,7.79 18,7.2 18,6.5C18,5.83 17.76,5.23 17.27,4.73C16.79,4.23 16.2,4 15.5,4C14.83,4 14.23,4.23 13.73,4.73C13.23,5.23 13,5.83 13,6.5C13,7.2 13.23,7.79 13.73,8.27C14.23,8.76 14.83,9 15.5,9M19.31,8.91L22.41,12L21,13.41L17.86,10.31C17.08,10.78 16.28,11 15.47,11C14.22,11 13.16,10.58 12.3,9.7C11.45,8.83 11,7.77 11,6.5C11,5.27 11.45,4.2 12.33,3.33C13.2,2.45 14.27,2 15.5,2C16.77,2 17.83,2.45 18.7,3.33C19.58,4.2 20,5.27 20,6.5C20,7.33 19.78,8.13 19.31,8.91M16.5,18H5.5L8.25,14.5L10.22,16.83L12.94,13.31L16.5,18M18,13L20,15V20C20,20.55 19.81,21 19.41,21.4C19,21.79 18.53,22 18,22H4C3.45,22 3,21.79 2.6,21.4C2.21,21 2,20.55 2,20V6C2,5.47 2.21,5 2.6,4.59C3,4.19 3.45,4 4,4H9.5C9.2,4.64 9.03,5.31 9,6H4V20H18V13Z"
+                />
+              </svg>
+            </a>
           </div>
 
           <!-- Task Deliverable -->
@@ -299,11 +316,11 @@ export default {
   created: function() {
     var j; /* jobs */
     var t; /* tasks */
-    this.showTasks( j, t );
-    this.showDeliverables( j, t );
+    this.checkTasks(j, t);
+    this.checkDeliverables(j, t);
   },
   methods: {
-    showTasks: function( j, t ) {
+    checkTasks: function(j, t) {
       if (this.tasks === "show") {
         for (j = 0; j < this.jobs.length; j++) {
           for (t = 0; t < this.jobs[j].tasks.length; t++) {
@@ -318,7 +335,18 @@ export default {
         }
       }
     },
-    showDeliverables: function( j, t ) {
+    toggleTasks: function(j, t) {
+      for (j = 0; j < this.jobs.length; j++) {
+        for (t = 0; t < this.jobs[j].tasks.length; t++) {
+          if (this.jobs[j].tasks[t].showTask) {
+            this.jobs[j].tasks[t].showTask = false;
+          } else {
+            this.jobs[j].tasks[t].showTask = true;
+          }
+        }
+      }
+    },
+    checkDeliverables: function(j, t) {
       if (this.deliverables === "show") {
         for (j = 0; j < this.jobs.length; j++) {
           for (t = 0; t < this.jobs[j].tasks.length; t++) {
@@ -434,6 +462,10 @@ h6.tl-event-company {
   margin-left: 14px;
 }
 
+.tl-task-desc {
+  position: relative;
+}
+
 .tl-task-desc,
 .tl-task-deliverable-desc {
   margin-left: 104px;
@@ -447,5 +479,17 @@ h6.tl-event-company {
 
 .tl-task-deliverable-desc img {
   width: 100%;
+}
+
+.tl-task-desc-lit {
+  float: left;
+  margin-right: 50px;
+}
+
+.btn-view {
+  position: absolute;
+  float: right;
+  top: 0;
+  right: 0;
 }
 </style>
